d340e007a5d3cc9b9453e1f537eac2207c80fe64
theirix <theirix@gmail.com>
2017-07-21 00:22:01 +0300

    Updated style according to the conan new

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(username="theirix", channel="ci")
     builder.add_common_builds(shared_option_name="jsoncpp:shared", pure_c=False)
     for build in builder.builds:
         if build.settings["arch"] == "x86":
--- test/conanfile.py
+++ test/conanfile.py
@@ -1,28 +1,21 @@
 from conans import ConanFile, CMake
 import os
-from os import path
 
-# This easily allows to copy the package in other user or channel
-channel = os.getenv("CONAN_CHANNEL", "testing")
-username = os.getenv("CONAN_USERNAME", "theirix")
-pkg_version = "1.8.0"
-
-class DefaultNameConan(ConanFile):
-    name = "DefaultName"
-    version = "0.1"
+class JsoncppTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "jsoncpp/%s@%s/%s" % (pkg_version, username, channel)
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
+        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.build()
 
     def imports(self):
-        self.copy(pattern="*.dll", dst="bin", src="bin")
-        self.copy(pattern="*.so", dst="bin", src="lib")
-        self.copy(pattern="*.dylib", dst="bin", src="lib")
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.so", dst="bin", src="lib")        
+        self.copy("*.dylib*", dst="bin", src="lib")
 
     def test(self):
-        self.run(path.join(".", "testapp"), cwd="bin")
+        os.chdir("bin")
+        self.run(".%sexample" % os.sep)
