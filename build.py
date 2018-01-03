ab40356b0ec31befb229c7017ca595c87ad4b86b
theirix <theirix@gmail.com>
2018-01-03 23:02:14 +0300

    Fixed build.py

--- build.py
+++ build.py
@@ -1,9 +1,16 @@
 from conan.packager import ConanMultiPackager
+import copy
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="jsoncpp:shared", pure_c=False)
-    for build in builder.builds:
-        if build.settings["arch"] == "x86":
-            build.settings["cmake_installer:arch"] = "x86_64"
+
+    builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        new_settings = copy.copy(settings)
+        if new_settings["arch"] == "x86":
+            new_settings["cmake_installer:arch"] = "x86_64"
+        builds.append([new_settings, options, env_vars, build_requires])
+    builder.builds = builds
+
     builder.run()
