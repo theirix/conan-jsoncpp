7ff32e1ff0d7a4fb9d79746d1feef09735488159
theirix <theirix@gmail.com>
2018-01-08 12:44:20 +0300

    Renamed example

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -21,11 +21,10 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "example")
+            bin_path = os.path.join("bin", "test_package")
             if self.settings.os == "Windows":
                 self.run(bin_path)
             elif self.settings.os == "Macos":
                 self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
                 self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
-
