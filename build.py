b973c9f995b81446a9d8d3a8509ba0ad0d9c9b14
theirix <theirix@gmail.com>
2018-01-12 13:54:58 +0300

    Dropped adjusting cmake_installer arch from CI

--- build.py
+++ build.py
@@ -5,12 +5,4 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="jsoncpp:shared", pure_c=False)
 
-    builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        new_settings = copy.copy(settings)
-        if new_settings["arch"] == "x86":
-            new_settings["cmake_installer:arch"] = "x86_64"
-        builds.append([new_settings, options, env_vars, build_requires])
-    builder.builds = builds
-
     builder.run()
