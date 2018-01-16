0128a640a0d9c67563bd4cc144863435e5f09cdf
theirix <theirix@gmail.com>
2018-01-16 16:48:59 +0300

    Exclude mingw x86 CI builds

--- build.py
+++ build.py
@@ -1,8 +1,16 @@
 from conan.packager import ConanMultiPackager
 import copy
+import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="jsoncpp:shared", pure_c=False)
+
+    builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        # skip mingw cross-builds
+        if not (platform.system() == "Windows" and settings["compiler"] == "gcc" and settings["arch"] == "x86"):
+            builds.append([settings, options, env_vars, build_requires])
+    builder.builds = builds
 
     builder.run()
