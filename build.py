bcf8790903a4102ddbe86e3abe5511d621b203c9
theirix <theirix@gmail.com>
2019-05-30 01:35:44 +0300

    Update deprecated CPT builds

--- build.py
+++ build.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="jsoncpp:shared", pure_c=False)
 
-    builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
+    items = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
         # skip mingw cross-builds
         if not (platform.system() == "Windows" and settings["compiler"] == "gcc" and settings["arch"] == "x86"):
-            builds.append([settings, options, env_vars, build_requires])
-    builder.builds = builds
+            items.append([settings, options, env_vars, build_requires, reference])
+    builder.items = items
 
     builder.run()
